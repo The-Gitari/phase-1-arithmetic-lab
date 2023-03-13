0d7d0d55936366e1e3d741139c05d30cbbc619ea
Noel Gitari <gitarinoel@gmail.com>
2023-03-14 01:40:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
+//test 1
 // Write your code here
+let num1 = 31;
+let num2 =2;
+let multiply = num1 * num2;
+// test 2
+let random = Math.floor (Math.random() * 10) +1;
+// test 3
+let num3= 104;
+let num4= 5;
+let mod= num3 % num4;
+//test 4
+let max= Math.max(0,5,10,15,20);
